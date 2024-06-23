a525210bfc973894087b4dba1f710539d840bd8e
Sachin-Tharaka <tharakasachin98@gmail.com>
2024-06-23 23:19:16 +0530

    minor changes

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,34 +1,40 @@
-import React from 'react';
-import { View, TouchableOpacity, StyleSheet } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import { useNavigation } from '@react-navigation/native';
+import React from "react";
+import { View, TouchableOpacity, StyleSheet } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { useNavigation } from "@react-navigation/native";
 
 const Navbar = () => {
+  const navigation = useNavigation();
 
-    const navigation = useNavigation();
+  const handleHome = () => {
+    navigation.navigate("BookingHouse");
+  };
 
-    const handleHome = () => {
-         navigation.navigate('BookingHouse');
-      };
-      
-      const handleNotifications = () => {
-        navigation.navigate('NotificationScreen');
-      };
-      
-      const handleAccount = () => {
-        navigation.navigate('UserAccount');
-      };
-      
-      const handleGallery = () => {
-        navigation.navigate('Home');
-      };
-      
-      return (
-        <View style={styles.navBar}>
+  const handleNotifications = () => {
+    navigation.navigate("NotificationScreen");
+  };
+
+  const handleAccount = () => {
+    navigation.navigate("UserAccount");
+  };
+
+  const handleGallery = () => {
+    navigation.navigate("Home");
+  };
+
+  const handleChats = () => {
+    navigation.navigate("ChatList");
+  };
+
+  return (
+    <View style={styles.navBar}>
       <TouchableOpacity onPress={handleHome} style={styles.iconContainer}>
         <Icon name="home" size={24} color="#333" />
       </TouchableOpacity>
-      <TouchableOpacity onPress={handleNotifications} style={styles.iconContainer}>
+      <TouchableOpacity
+        onPress={handleNotifications}
+        style={styles.iconContainer}
+      >
         <Icon name="bell" size={24} color="#333" />
       </TouchableOpacity>
       <TouchableOpacity onPress={handleAccount} style={styles.iconContainer}>
@@ -37,26 +43,28 @@ const Navbar = () => {
       <TouchableOpacity onPress={handleGallery} style={styles.iconContainer}>
         <Icon name="navicon" size={24} color="#333" />
       </TouchableOpacity>
+      <TouchableOpacity onPress={handleChats} style={styles.iconContainer}>
+        <Icon name="comment" size={24} color="#333" />
+      </TouchableOpacity>
     </View>
-      );
-  };
-  
-  const styles = StyleSheet.create({
-    navBar: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      backgroundColor: '#fff',
-      height: 60,
-      paddingHorizontal: 16, 
-      width: '100%', // Make the navigation bar span the full width
-    },
-    iconContainer: {
-      alignItems: 'center',
-      marginLeft:35,
-      marginRight:35
-    },
-  });
-  
-  export default Navbar;
-  
+  );
+};
+
+const styles = StyleSheet.create({
+  navBar: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    height: 60,
+    paddingHorizontal: 8,
+    width: "100%", // Make the navigation bar span the full width
+  },
+  iconContainer: {
+    alignItems: "center",
+    marginLeft: 35,
+    marginRight: 35,
+  },
+});
+
+export default Navbar;
