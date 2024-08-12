5e96f6c7ae7666b92a96e801d53e23a7825cb401
Sachin Tharaka <60689075+Sachin-Tharaka@users.noreply.github.com>
2024-08-12 09:04:42 +0530

    fix status bar visibility (#27)

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
+import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import WelcomeScreen from "./screens/WelcomeScreen";
@@ -153,6 +154,7 @@ export default function App() {
 
   return (
     <NavigationContainer>
+      <StatusBar style="dark" />
       <Stack.Navigator initialRouteName="Welcome">
         <Stack.Screen
           name="Welcome"
