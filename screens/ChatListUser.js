5f2a7f3e6f1e56055ac6af392df6da445650e2a2
Sachin-Tharaka <tharakasachin98@gmail.com>
2024-08-14 00:13:56 +0530

    chat name

--- screens/ChatListUser.js
+++ screens/ChatListUser.js
@@ -17,7 +17,10 @@ const ChatListUser = ({ navigation }) => {
       try {
         const token = await AsyncStorage.getItem("token");
         const userId = await AsyncStorage.getItem("userId");
-        const chatPreview = await ChatService.fetchMessagePreviews(token, userId);
+        const chatPreview = await ChatService.fetchMessagePreviews(
+          token,
+          userId
+        );
         setChatPreview(chatPreview);
       } catch (error) {
         console.error("Failed to fetch chat previews:", error);
@@ -25,28 +28,29 @@ const ChatListUser = ({ navigation }) => {
     };
 
     fetchChatPreview().then(() => console.log("ChatListUser fetchChatPreview"));
-  }, []);  // Added dependency array to run the effect only once on mount
+  }, []); // Added dependency array to run the effect only once on mount
 
   const renderChatItem = ({ item }) => (
-      <TouchableOpacity
-          style={styles.chatItem}
-          onPress={() =>
-              navigation.navigate("ChatScreenUser", { chatId: item.chatThreadId })
-          }
-      >
-        <Text style={styles.chatName}>{item.chatThreadName}</Text>
-        <Text style={styles.lastMessage}>{item.lastMessage.message}</Text>
-      </TouchableOpacity>
+    <TouchableOpacity
+      style={styles.chatItem}
+      onPress={() =>
+        navigation.navigate("ChatScreenUser", { chatId: item.chatThreadId })
+      }
+    >
+      <Text style={styles.chatName}>{item.chatThreadName}</Text>
+      <Text style={styles.lastMessage}>{item.lastMessage.message}</Text>
+    </TouchableOpacity>
   );
 
   return (
-      <View style={styles.container}>
-        <FlatList
-            data={chatPreview}
-            keyExtractor={(item) => item.chatThreadId}  // Ensure this matches the correct field in your data
-            renderItem={renderChatItem}
-        />
-      </View>
+    <View style={styles.container}>
+      <Text style={styles.header}>Chats</Text>
+      <FlatList
+        data={chatPreview}
+        keyExtractor={(item) => item.chatThreadId} // Ensure this matches the correct field in your data
+        renderItem={renderChatItem}
+      />
+    </View>
   );
 };
 
@@ -55,6 +59,13 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 10,
     backgroundColor: "#f5f5f5",
+    paddingTop: 60,
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 40,
+    textAlign: "center",
   },
   chatItem: {
     padding: 15,
